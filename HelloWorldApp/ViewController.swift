f66857e922d8d1dc3750e58f17130c74c138b7b6
Ramazan <r-sh-94@mail.ru>
2022-03-02 15:51:18 +0300

    pressed button

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    @IBOutlet weak var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden = true
+        greetingButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func greetingButtonPressed() {
+        print("Button pressed")
+    }
+    
 }
 
